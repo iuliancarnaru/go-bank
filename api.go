1ddc65ea057663a6237db85395d90fdb3db925ae
Iulian Carnaru <iulian.carnaru@gmail.com>
2023-08-30 21:16:08 +0100

    jwt

--- api.go
+++ api.go
@@ -5,8 +5,11 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"strconv"
+	"time"
 
+	jwt "github.com/golang-jwt/jwt/v4"
 	"github.com/gorilla/mux"
 )
 
@@ -46,7 +49,7 @@ func (s *APIServer) Run() {
 	router := mux.NewRouter()
 
 	router.HandleFunc("/account", makeHTTPHandleFunc(s.handleAccount))
-	router.HandleFunc("/account/{id}", makeHTTPHandleFunc(s.handleGetAccountByID))
+	router.HandleFunc("/account/{id}", withJWTAuth(makeHTTPHandleFunc(s.handleGetAccountByID), s.store))
 	router.HandleFunc("/transfer", makeHTTPHandleFunc(s.handleTransfer))
 
 	log.Println("JSON API server running on port", s.listenAddr)
@@ -109,6 +112,13 @@ func (s *APIServer) handleCreateAccount(w http.ResponseWriter, r *http.Request) 
 		return err
 	}
 
+	tokenStr, err := createJWT(account)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("token", tokenStr)
+
 	return writeJSON(w, http.StatusOK, account)
 }
 
@@ -144,4 +154,69 @@ func getID(r *http.Request) (int, error) {
 	}
 
 	return id, nil
+}
+
+func permissionDenied(w http.ResponseWriter) {
+	writeJSON(w, http.StatusForbidden, APIError{Error: "permission denied"})
+}
+
+func withJWTAuth(handlerFunc http.HandlerFunc, s Storage) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("calling jwt auth middleware")
+		tokenStr := r.Header.Get("x-jwt-token")
+		token, err := validateJWT(tokenStr)
+		if err != nil {
+			permissionDenied(w)
+			return
+		}
+		if !token.Valid {
+			permissionDenied(w)
+			return
+		}
+
+		userID, err := getID(r)
+		if err != nil {
+			permissionDenied(w)
+			return
+		}
+
+		account, err := s.GetAccountById(userID)
+		if err != nil {
+			permissionDenied(w)
+			return
+		}
+
+		claims := token.Claims.(jwt.MapClaims)
+
+		if account.Number != int64(claims["accountNumber"].(float64)) {
+			permissionDenied(w)
+			return
+		}
+
+		handlerFunc(w, r)
+	}
+}
+
+func createJWT(account *Account) (string, error) {
+	claims := &jwt.MapClaims{
+		"expiresAt":     jwt.NewNumericDate(time.Now().Add(1 * time.Hour)),
+		"accountNumber": account.Number,
+	}
+
+	jwtSecret := os.Getenv("JWT_SECRET")
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	return token.SignedString([]byte(jwtSecret))
+}
+
+func validateJWT(tokenStr string) (*jwt.Token, error) {
+	jwtSecret := os.Getenv("JWT_SECRET")
+
+	return jwt.Parse(tokenStr, func(t *jwt.Token) (interface{}, error) {
+		if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", t.Header["alg"])
+		}
+
+		return []byte(jwtSecret), nil
+	})
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/iuliancarnaru/gobank
 go 1.21.0
 
 require (
-	github.com/gorilla/mux v1.8.0 // indirect
-	github.com/lib/pq v1.10.9 // indirect
+	github.com/golang-jwt/jwt/v4 v4.5.0
+	github.com/gorilla/mux v1.8.0
+	github.com/joho/godotenv v1.5.1
+	github.com/lib/pq v1.10.9
 )
